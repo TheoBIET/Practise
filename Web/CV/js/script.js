3c92cb4c55cc0271fe9350adb5a9a5e853bb352b
Théo <dev.theobiet@gmail.com>
2021-02-17 21:41:34 +0100

    add web

---
+++ Web/CV/js/script.js
@@ -0,0 +1,12 @@
+$(() => {
+
+      $(".navbar a, footer a").on("click", function(event){
+      
+          event.preventDefault();
+          var hash = this.hash;
+          
+          $('body,html').animate({scrollTop: $(hash).offset().top} , 400 , function(){window.location.hash = hash;})
+          
+      });
+  
+  })
